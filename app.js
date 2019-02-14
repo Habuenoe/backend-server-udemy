a43462b6a448d99af88d70732194caa82d4e859c
Hector Bueno <hectorbuenoenero@gmail.com>
2019-02-14 12:31:06 +0100

    Usuario y token listo

--- app.js
+++ app.js
@@ -4,11 +4,25 @@ var express = require('express');
 // Mongoose establecer conexión
 var mongoose = require('mongoose');
 
+// Body Parser
+var bodyParser = require('body-parser');
+
 // Inicializar variables
 var app = express();
 
-// Conexión a la BD
+// Body parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
 
+// Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
+
+
+// Conexión a la BD
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res)=>{
 
 if(err) throw err;
@@ -20,14 +34,19 @@ console.log('Base de datos:\x1b[32m%s\x1b[0m ',' online');
 });
 
 
-// Rutas
-app.get('/',(req , res , next ) => {
+// // Rutas  ( lo hemos copiado en el archivo app.js de la carpeta routes)
+// app.get('/',(req , res , next ) => {
 
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Petición realizada correctamente'
-    });
-});
+//     res.status(200).json({
+//         ok: true,
+//         mensaje: 'Petición realizada correctamente'
+//     });
+// });
+
+// Rutas
+app.use('/usuario' , usuarioRoutes);
+app.use('/login' , loginRoutes);
+app.use('/' , appRoutes);
 
 // Escuchar peticiones
 app.listen(3000,() =>{
